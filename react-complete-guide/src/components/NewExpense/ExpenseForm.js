26e8d66b8cb872cf13fd9e45d6bd586b19334160
Henry Li <henry226@bu.edu>
2021-11-21 19:19:27 -0500

    Add two way binding

--- react-complete-guide/src/components/NewExpense/ExpenseForm.js
+++ react-complete-guide/src/components/NewExpense/ExpenseForm.js
@@ -43,6 +43,9 @@ function ExpenseForm() {
       date: new Date(enteredDate)
     };
     console.log(expenseData);
+    setEnteredtitle('');
+    setEnteredAmount('');
+    setEnteredDate('');
   }
 
   return (
@@ -50,7 +53,7 @@ function ExpenseForm() {
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input type="text" value={enteredTitle} onChange={titleChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
@@ -58,6 +61,7 @@ function ExpenseForm() {
             type="number"
             min="0.01"
             step="0.01"
+            value={enteredAmount}
             onChange={amountChangeHandler}
           />
         </div>
@@ -67,6 +71,7 @@ function ExpenseForm() {
             type="date"
             min="2019-01-01"
             max="2022-12-31"
+            value={enteredDate}
             onChange={dateChangeHandler}
           />
         </div>
