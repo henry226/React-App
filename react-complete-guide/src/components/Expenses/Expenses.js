07dab3ec80859736e00d64aa5de394ef347f8f93
Henry Li <henry226@bu.edu>
2021-12-05 15:25:50 -0500

    Adding key

--- react-complete-guide/src/components/Expenses/Expenses.js
+++ react-complete-guide/src/components/Expenses/Expenses.js
@@ -16,7 +16,7 @@ function Expenses(props) {
   return (
     <Card className="expenses">
       <ExpensesFilter initialYear={filteredYear} onChangeFilter={filterChangeHandler} />
-      {props.items.map(expense => <ExpenseItem title={expense.title} amount={expense.amount} date={expense.date} />)}
+      {props.items.map(expense => <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />)}
     </Card>
   );
 }
